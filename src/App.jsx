cc23374f375c843f4154c72521c617cdff617b6a
axelbriandip <axelbriandip.rg@gmail.com>
2022-06-23 15:11:30 -0300

    posRandom y colorRandom hechos (posRandom con falla cuando coinciden pos/posRandom)

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,30 @@ import './styles.css';
 import users from './users.json';
 
 function App() {
+  // random entre 0 y 9
+  const min = 0;
+  const max = users.length-1;
+  const posRandom = Math.floor(Math.random() * (max-min + 1)) + min;
+
+  // colores random
+  const colorRandom = Math.floor(Math.random()*16777215).toString(16);
+
+  // useState
   const [pos, setPos] = useState(0);
   const cambiarUsuario = () => {
-    if(pos!=9) setPos(pos+1);
+      setPos(posRandom)
   }
-  const nombreCompleto = users[pos].name.title + " " + users[pos].name.first + " " + users[pos].name.last;
-  const url_foto = users[pos].picture.large;
+
+  // resultados
+  const nombreCompleto = users[pos].name.title + " " + users[pos].name.first + " " + users[pos].name.last + " pos: " + pos;
+  const foto = users[pos].picture.large;
 
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor: `#${colorRandom}`}}>
       <h1>{nombreCompleto}</h1>
-      <img src={url_foto} alt="foto"/>
+      <img src={foto} alt="foto"/>
       <button onClick={cambiarUsuario}>Cambiar user</button>
+      <span>{posRandom}</span>
     </div>
   )
 }
