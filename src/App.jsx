1c8cc9a292267ab426f3eccb06047abce7567d7f
axelbriandip <axelbriandip.rg@gmail.com>
2022-06-23 14:39:45 -0300

    cambiar al usuario siguiente (hecho)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import './styles.css'
+import { useState } from 'react';
+import './styles.css';
+import users from './users.json';
 
 function App() {
+  const [pos, setPos] = useState(0);
+  const cambiarUsuario = () => {
+    if(pos!=9) setPos(pos+1);
+  }
+  const nombreCompleto = users[pos].name.title + " " + users[pos].name.first + " " + users[pos].name.last;
+  const url_foto = users[pos].picture.large;
+
   return (
     <div className="App">
+      <h1>{nombreCompleto}</h1>
+      <img src={url_foto} alt="foto"/>
+      <button onClick={cambiarUsuario}>Cambiar user</button>
     </div>
   )
 }
 
-export default App
+export default App;
