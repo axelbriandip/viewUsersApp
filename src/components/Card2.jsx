e2d781fa4051638433d878e1ff3f40f7d08dc74a
axelbriandip <axelbriandip.rg@gmail.com>
2022-06-24 08:34:32 -0300

    agregué 'index' dentro del return de 'Card2'. Solo falta agregar íconos.

--- src/components/Card2.jsx
+++ src/components/Card2.jsx
@@ -2,24 +2,20 @@ import React, { useState } from 'react';
 import users from '../users.json';
 
 const Card2 = () => {
-    // rnúmero andom entre 0 y 9
-    const min = 0;
-    const max = users.length-1;
-    const pos = Math.floor(Math.random() * (max-min + 1)) + min;
-
-    // color random
+    const random = Math.floor(Math.random() * users.length);
     const color = Math.floor(Math.random()*16777215).toString(16);
 
-    const [pos1, setPos1] = useState(0);
-    const modificar = () => {
-        setPos1(pos);
+    const [index, setIndex] = useState(random);
+    const cambiarUsuario = () => {
+        const random2 = Math.floor(Math.random() * users.length);
+        setIndex(random2);
     }
 
-    const nombreCompleto = `${users[pos].name.title} ${users[pos].name.first} ${users[pos].name.last}`;
-    const foto = users[pos].picture.large;
-    const email = users[pos].email;
-    const telefono = users[pos].phone;
-    const ubicacion = `${users[pos].location.city}, ${users[pos].location.country}`;
+    const nombreCompleto = `${users[index].name.title} ${users[index].name.first} ${users[index].name.last}`;
+    const foto = users[index].picture.large;
+    const email = users[index].email;
+    const telefono = users[index].phone;
+    const ubicacion = `${users[index].location.city}, ${users[index].location.country}`;
 
     return (
         <div className='App2' style={{backgroundColor: `#${color}`}}>
@@ -33,7 +29,7 @@ const Card2 = () => {
                     <span style={{color: `#${color}`}}>{telefono}</span>
                     <span style={{color: `#${color}`}}>{ubicacion}</span>
                 </div>
-                <div className="boton-cambiar" onClick={modificar} style={{backgroundColor: `#${color}`}}>C</div>
+                <div className="boton-cambiar" onClick={cambiarUsuario} style={{backgroundColor: `#${color}`}}>C</div>
             </div>
         </div>
     );
